df33de817df62392eeae65f256edf6bcad9536fb
HunterX-7 <fraquin7@gmail.com>
2024-12-10 00:29:09 -0500

    ng generate component features/profile/user-profile --style=scss

---
+++ client/src/app/features/profile/user-profile/user-profile.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { User } from '../../../core/models/user.model';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+  user: User | null = null;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.getCurrentUser();
+  }
+}
